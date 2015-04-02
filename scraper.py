52a14ac1357720cee5e583714bb3b237ba94b87a
taenin <persons.matthew@gmail.com>
2015-04-02 03:13:06 -0400

    Shortened the scrape time significantly

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import updater
 def main(args):
 	startTime = int(args[1])
 	mainWorker = updater.Worker(startTime)
-	comThread = timers.BackgroundUpdater(3, mainWorker.updateInformation).start()
+	comThread = timers.BackgroundUpdater(1.5, mainWorker.updateInformation).start()
 	try:
 		while True:
 			time.sleep(1)
